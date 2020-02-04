56bf9c671a7a493c9c584b7dcda5c52abdeb99a7
Zachary Ernst <zac.ernst@gmail.com>
2020-02-04 19:54:24 +0000

    black

--- example.py
+++ example.py
@@ -4,12 +4,11 @@ from flask_wrap import FlaskWrap
 
 logging.basicConfig(level=logging.INFO)
 
+
 class HelloWorldService(FlaskWrap):
-
     def endpoint_hello_world(self, name=None):
-        logging.info('called endpoint_hello_world')
-        return {'response': f'hello, {name}!'}
-
+        logging.info("called endpoint_hello_world")
+        return {"response": f"hello, {name}!"}
 
 
 hello_world_service = HelloWorldService()
